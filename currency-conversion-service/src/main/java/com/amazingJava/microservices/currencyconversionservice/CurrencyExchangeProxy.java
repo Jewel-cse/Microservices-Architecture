d641a9bd45cd35534d59722c281c1e50aa33edf4
Jewel-cse <md.jewel.cse@gmail.com>
2024-05-18 13:29:11 +0600

    call any microservice which is registered with eureka though api gateway

--- currency-conversion-service/src/main/java/com/amazingJava/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/amazingJava/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -1,7 +1,6 @@
 package com.amazingJava.microservices.currencyconversionservice;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
