4d1425dbf191594fb81f57df40152877bec25749
Jewel-cse <md.jewel.cse@gmail.com>
2024-05-19 11:52:09 +0600

    use of circuit breaker and rate limiter

--- currency-exchange-service/src/main/java/com/amazingJava/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/amazingJava/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,5 +1,7 @@
 package com.amazingJava.microservices.currencyexchangeservice;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,11 +15,14 @@ public class CircuitBreakerController {
 
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
     @GetMapping("/sample-api")
-    @Retry(name="sample-api",fallbackMethod = "fallback")
+    //@Retry(name="sample-api",fallbackMethod = "fallback")
+    //@CircuitBreaker(name="default",fallbackMethod = "fallback")
+    @RateLimiter(name = "default")
     public String SampleApi(){
         logger.info("sample api call received");
-        ResponseEntity<String> entity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy",String.class);
-        return entity.getBody();
+        //ResponseEntity<String> entity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy",String.class);
+        //return entity.getBody();
+        return "sample api";
     }
     public String fallback(Exception ex){
         return "fallback method called";
